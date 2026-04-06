a170e9e741553ecf48792c5a20f639765efbaa12
Madhukarnarra1837 <madhukarnarra03081996@gmail.com>
2026-04-06 18:42:02 +0530

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    // This tells Jest to ignore CSS/Style imports
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/styleMock.js',
+    // If you have image imports (png, svg, jpg), add this too:
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/styleMock.js',
+  },
+};
